1878bbcbc4e191166a6d161764a647b41c6d1e27
gittelegram12 <bhattisalman887@gmail.com>
2025-06-06 08:11:27 +0500

    Update monitoring logic with webhook

--- telegram_monitor.py
+++ telegram_monitor.py
@@ -8,7 +8,7 @@ api_id = 20295619
 api_hash = "1f7150b62cc6f2cf1c38f0855719272c"
 string_session = os.getenv("STRING_SESSION")  # safer than storing login
 channel_username = "@PocketSignalsM1"
-webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook-test/accada36-9029-4db0-88d6-23b02bec865d")
+webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook/accada36-9029-4db0-88d6-23b02bec865d")
 
 client = TelegramClient(StringSession(string_session), api_id, api_hash)
 sequence = []
