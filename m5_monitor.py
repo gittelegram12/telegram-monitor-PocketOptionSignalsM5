3c5a996dd57e44a1c40be53dc3fd47930c2b4516
gittelegram12 <bhattisalman887@gmail.com>
2025-06-07 01:51:40 +0500

    Fix: Update Webhook URL

--- m5_monitor.py
+++ m5_monitor.py
@@ -8,7 +8,7 @@ api_id = 20295619
 api_hash = "1f7150b62cc6f2cf1c38f0855719272c"
 string_session = os.getenv("STRING_SESSION")
 channel_username = "@PocketOptionSignalsM5"
-webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook-test/fd2ddf25-4b6c-4d7b-9ee1-0d927fda2a41")
+webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook/fd2ddf25-4b6c-4d7b-9ee1-0d927fda2a41")
 
 client = TelegramClient(StringSession(string_session), api_id, api_hash)
 sequence = []
