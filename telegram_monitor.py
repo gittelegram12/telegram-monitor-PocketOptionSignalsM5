5e7f74d9326b7a62fa1ae938e8cc869462a351ee
gittelegram12 <bhattisalman887@gmail.com>
2025-06-06 14:42:47 +0500

    Initial commit for PocketOptionSignalsM5 monitoring

--- telegram_monitor.py
+++ telegram_monitor.py
@@ -7,8 +7,8 @@ import requests
 api_id = 20295619
 api_hash = "1f7150b62cc6f2cf1c38f0855719272c"
 string_session = os.getenv("STRING_SESSION")
-channel_username = "@PocketSignalsM1"
-webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook/accada36-9029-4db0-88d6-23b02bec865d")
+channel_username = "@PocketOptionSignalsM5"
+webhook_url = os.getenv("WEBHOOK_URL", "https://marisbriedis.app.n8n.cloud/webhook-test/fd2ddf25-4b6c-4d7b-9ee1-0d927fda2a41")
 
 client = TelegramClient(StringSession(string_session), api_id, api_hash)
 sequence = []
